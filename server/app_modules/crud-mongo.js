8aab08c61f95b618739318db6e8d1c014ae7c6c5
Chatti Nader <chatti.tkd@gmail.com>
2020-04-09 23:48:51 +0200

    Count et Récupération des données de la collection cas_pub

--- server/app_modules/crud-mongo.js
+++ server/app_modules/crud-mongo.js
@@ -1,12 +1,13 @@
 var MongoClient = require('mongodb').MongoClient;
 var assert = require('assert');
 
-// Connection URL
+// URL de connexion
 const url = 'mongodb+srv://lemurien:lemurien@lemurien-f2pky.mongodb.net/test?retryWrites=true&w=majority';
 
-// Database Name
+// Nom de la base de données
 const dbName = 'lemurien';
 
+// Connexion à la base distante MongoDB Atlas
 exports.connexionMongo = function (callback) {
     MongoClient.connect(url, function (err, client) {
         var db = client.db(dbName);
@@ -15,3 +16,69 @@ exports.connexionMongo = function (callback) {
         callback(err, db);
     });
 }
+
+// Count de données dans la collection cas_pub
+exports.countCasPub = function (name, callback) {
+    MongoClient.connect(url, function (err, client) {
+        var db = client.db(dbName);
+
+        console.log("db " + db)
+        if (!err) {
+            if (name == '') {
+                db.collection('cas_pub')
+                    .count()
+                    .then(rep => callback(rep));
+            } else {
+                let query = {
+                    "name": { $regex: ".*" + name + ".*", $options: "i" }
+                }
+                db.collection('cas_pub')
+                    .find(query)
+                    .count()
+                    .then(rep => callback(rep));
+            }
+        }
+    });
+};
+
+// Récupération des cas
+exports.findCasPub = function (page, pagesize, name, callback) {
+    MongoClient.connect(url, function (err, client) {
+        var db = client.db(dbName);
+
+        console.log("db " + db)
+        if (!err) {
+            if (name == '') {
+                db.collection('cas_pub')
+                    .find()
+                    .skip(page * pagesize)
+                    .limit(pagesize)
+                    .toArray()
+                    .then(arr => {
+                        db.collection('cas_pub')
+                            .count()
+                            .then(rep => callback(arr, rep))
+                    });
+            }
+            else {
+                let query = {
+                    "name": { $regex: ".*" + name + ".*", $options: "i" }
+                }
+                db.collection('cas_pub')
+                    .find(query)
+                    .skip(page * pagesize)
+                    .limit(pagesize)
+                    .toArray()
+                    .then(arr => {
+                        db.collection('cas_pub')
+                            .find(query)
+                            .count()
+                            .then(rep => callback(arr, rep))
+                    });
+            }
+        }
+        else {
+            callback(-1);
+        }
+    });
+};
--- server/server.js
+++ server/server.js
@@ -11,6 +11,7 @@ app.use(express.static(__dirname + '/public'));
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
+// Cross Origin Ressources
 app.use(function (req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
@@ -27,7 +28,7 @@ app.get('/', function (req, res) {
     res.sendFile(__dirname + '/public/index.html');
 });
 
-// connexion à la base
+// Connexion à la base distante MongoDB Atlas
 app.get('/api/connection', function (req, res) {
     mongoDBModule.connexionMongo(function (err, db) {
         let reponse;
@@ -43,5 +44,24 @@ app.get('/api/connection', function (req, res) {
             }
         }
         res.send(JSON.stringify(reponse));
+    });
+});
+
+// Récupération des cas
+app.get('/api/cas', function (req, res) {
+    let page = parseInt(req.query.page || 1);
+    let pagesize = parseInt(req.query.pagesize || 10);
+    let name = req.query.name || '';
+
+    mongoDBModule.findCasPub(page, pagesize, name, function (data, count) {
+        var objdData = {
+            msg: "Cas_Pub recherchés avec succès",
+            data: data,
+            count: count,
+            page: page,
+            pagesize: pagesize
+        }
+        
+        res.send(JSON.stringify(objdData));
     });
 });
