a566c24473d00d219fc91956f6e4b50a2a08439b
JrHamza <hamza.jrad@gmail.com>
2020-04-09 17:02:53 +0200

    Ajout du composant Observation + routeur de navigation

--- src/App.js
+++ src/App.js
@@ -1,14 +1,21 @@
 import React from 'react'
 import './App.css'
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 import Navbar from './components/Navbar'
 import Home from './components/Home'
+import Observation from './components/Observation'
 
 function App() {
     return (
-        <div className="App">
-          <Navbar />
-          <Home />
-        </div>
+        <Router>
+            <div className="App">
+                <Navbar />
+                <Switch>
+                    <Route path="/" exact component={Home} />
+                    <Route path="/observation/" exact component={Observation} />
+                </Switch>
+            </div>
+        </Router>
     );
 }
 
---
+++ src/components/Observation.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+
+export default class Observation extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+
+        }
+    }
+
+    render() {
+        return (
+            <div>
+                <h1>Observation</h1>
+            </div>
+        );
+    }
+}
