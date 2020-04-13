265edf3cbe40c47f8ec6fc6230a66e02a8ccce1a
JrHamza <hamza.jrad@gmail.com>
2020-04-13 15:30:03 +0200

    Ajout de la route vers page stats

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -6,6 +6,8 @@ import Typography from '@material-ui/core/Typography';
 import HomeIcon from '@material-ui/icons/Home';
 import { Link } from 'react-router-dom';
 import { Grid } from '@material-ui/core';
+import EqualizerIcon from '@material-ui/icons/Equalizer';
+import InfoIcon from '@material-ui/icons/Info';
 
 export default class Navbar extends Component {
   constructor(props) {
@@ -21,19 +23,43 @@ export default class Navbar extends Component {
         <AppBar position="static" style={{ background: '#282c34' }}>
           <Toolbar>
             <Grid container spacing={2}>
-              <Grid item xs={2}>
-                <IconButton
-                  edge="start"
-                  color="inherit"
-                  aria-label="open drawer"
-                  style={{ marginRight: 5 }}
-                >
-                  <Link to={`/`} style={{ textDecoration: 'none', color: 'white' }}>
-                    <HomeIcon />
-                  </Link>
-                </IconButton>
+              <Grid item xs={3}>
+               <Grid container spacing={2}>
+                 <Grid item xs={6}>
+                   <IconButton
+                       edge="start"
+                       color="inherit"
+                       aria-label="open drawer"
+                       style={{ marginRight: 5 }}
+                   >
+                     <Link to={`/`} style={{ textDecoration: 'none', color: 'white' }}>
+                       <HomeIcon />
+                     </Link>
+                   </IconButton>
+                   <IconButton
+                       edge="start"
+                       color="inherit"
+                       aria-label="open drawer"
+                       style={{ marginRight: 5 }}
+                   >
+                     <Link to={`/Statistique`} style={{ textDecoration: 'none', color: 'white' }}>
+                       <EqualizerIcon />
+                     </Link>
+                   </IconButton>
+                   <IconButton
+                       edge="start"
+                       color="inherit"
+                       aria-label="open drawer"
+                       style={{ marginRight: 5 }}
+                   >
+                     <Link to={`/About`} style={{ textDecoration: 'none', color: 'white' }}>
+                       <InfoIcon />
+                     </Link>
+                   </IconButton>
+                 </Grid>
+               </Grid>
               </Grid>
-              <Grid item xs={8}>
+              <Grid item xs={6}>
                 <Typography variant="h6" noWrap>
                   Observation
             </Typography>
